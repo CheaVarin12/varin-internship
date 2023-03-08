23237b0bf1487e7b1b1ac772d371c69a76d7a9ef
Varin Chea <varin.chea@student.cadt.edu.kh>
2023-03-09 05:36:03 +0700

    Lesson 3

---
+++ Lesson 3-កំណត់​លក្ខខណ/index.php
@@ -0,0 +1,60 @@
+<?php
+//   $number = 100;
+//   if($number>100){
+//     echo "> 100";
+//   }
+//   if($number == 100){
+//     echo "=100";
+//   }
+//   else{
+//     echo "< 100";
+//   }
+
+// echo "<br>";
+
+//   if(false || false){
+//     echo "Yes";
+//   }else{
+//     echo "No";
+//   }
+//   echo "<br>";
+
+//   if(false || true){
+//     echo "Yes";
+//   }else{
+//     echo "No";
+//   }
+//   echo "<br>";
+
+//   if(false && false){
+//     echo "Yes";
+//   }else{
+//     echo "No";
+//   }
+//   echo "<br>";
+
+//   if(false && true){
+//     echo "Yes";
+//   }else{
+//     echo "No";
+//   }
+//   echo "<br>";
+//   if(true && true){
+//     echo "Yes";
+//   }else{
+//     echo "No";
+//   }
+
+$number =2018;
+
+if(strlen($number)==2){
+    echo 2;
+}
+else if (strlen($number)==3){
+    echo 3; 
+}
+else{
+    echo "blah blah blah";
+}
+   
+?>
