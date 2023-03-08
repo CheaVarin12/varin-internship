f43ec6790520d8f4b309a3fcec6d25b56646c75d
Varin Chea <varin.chea@student.cadt.edu.kh>
2023-03-09 04:36:12 +0700

    Lesson 1,2

---
+++ Lesson 1-អថេរ​និងលេខ/index.php
@@ -0,0 +1,36 @@
+<?php
+    $x=50;
+    $y=100;
+    $area = $x * $y;
+    echo $area;
+    echo "<br>";
+
+    // $number =10;
+    // echo $number;
+    // echo "<br>";
+    // $number =20;
+
+    // $number +=5;
+    // $number -=5;
+    // $number *=5;
+    // $number /=5;
+    // $number %=5;
+
+    // $number++;
+    // ++$number;
+    // $number--;
+    // --$number;
+
+    // echo $number;
+
+    // echo (int)(13/2);
+
+    // echo PHP_INT_MAX;
+    //  echo "<br>";
+    // echo PHP_INT_MIN;
+    
+     echo pow(2,10);
+
+
+
+?>
---
+++ Lesson 2-អក្សរ (string)/index.php
@@ -0,0 +1,18 @@
+<?php
+$name = "Khode";
+$fruit = "apple";
+$amount = 5;
+// $test= "Welcome to ".$name." Academy <br>";
+// $test= "I have $amount {$fruit}s";
+// $test= 'I\'m fine';
+$test = "It 's called \"Khode Academy\"";
+echo $test .'<br>';
+
+$text = "  Welcome to khode";
+// echo strrev($text)."<br>";
+// echo strtolower($text)."<br>";
+// echo trim($text);
+// echo strripos($text,"khode");
+// echo str_ireplace("khode","Cambodia", $text);
+echo str_repeat("khode",3);
+?>
